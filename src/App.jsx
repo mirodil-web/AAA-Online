5a36dd5fc0e6e2d76ff234f65a4028322174df7c
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2023-12-06 15:24:49 +0500

    Instal react router and page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+// import { useState } from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
+
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' >
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  )
   return (
     <div>
-
+      <div className=''>
+        <RouterProvider router={routes} />
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
